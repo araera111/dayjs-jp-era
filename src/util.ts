64c08c666cfe342a5bfae1d046663639b1a87938
yu <araera111@gmail.com>
2023-02-27 12:26:14 +0900

    feat(add date string Dayjs):

--- src/util.ts
+++ src/util.ts
@@ -1,29 +1,31 @@
+import dayjs, { Dayjs } from 'dayjs';
+
 export const dropLastString = (str: string) => str.slice(0, -1);
 
-export const getJpYear = (date: Date, isYear: boolean): string => {
+export const getJpYear = (date: Date | string | Dayjs, isYear: boolean): string => {
   let result: string;
   try {
-    const jpDate = Intl.DateTimeFormat("ja-JP-u-ca-japanese", {
-      year: "2-digit",
-      era: "long",
-    }).format(date);
+    const jpDate = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+      year: '2-digit',
+      era: 'long'
+    }).format(dayjs(date).toDate());
     result = isYear ? jpDate : dropLastString(jpDate);
   } catch {
-    result = "該当なし";
+    result = '該当なし';
   }
   return result;
 };
 
-export const getJpEra = (date: Date): string => {
+export const getJpEra = (date: Date | string | Dayjs): string => {
   let result: string;
   try {
-    result = Intl.DateTimeFormat("ja-JP-u-ca-japanese", {
-      era: "long",
+    result = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+      era: 'long'
     })
-      .format(date)
+      .format(dayjs(date).toDate())
       .slice(0, 2);
   } catch {
-    result = "不明";
+    result = '不明';
   }
   return result;
 };
